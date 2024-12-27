52023eec0618388167ac5c2e0a1ea1c86d8e1a84
takuto1127 <tiharu717@gmail.com>
2024-12-27 15:09:27 +0900

    早くならん

--- nqbt_seq.c
+++ nqbt_seq.c
@@ -2,84 +2,81 @@
  * 並列分散処理 第5回課題
  * Dec. 17th, 2024
  */
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #define MAX_N (28)
 
 int check_and_set(unsigned int mat[], int n, int row, int col)
 {
-	int i, j, c = 0;
+    int i, j, c = 0;
 
-	{
-	/* horizontal */
-	for (j = col - 1; j >= 0; j--) {
-		if (mat[j] & (1U << row)) {
-			return c;
-		}
-	}
-	}
+    /* horizontal */
+    for (j = col - 1; j >= 0; j--) {
+        if (mat[j] & (1U << row)) {
+            return c;
+        }
+    }
 
-	/* obliquely upward */
-	for (i = row - 1, j = col - 1; j >= 0 && i >= 0; j--, i--) {
-		if (mat[j] & (1U << i)) {
-			return c;
-		}
-	}
+    /* obliquely upward */
+    for (i = row - 1, j = col - 1; j >= 0 && i >= 0; j--, i--) {
+        if (mat[j] & (1U << i)) {
+            return c;
+        }
+    }
 
-	/* diagonally downward */
-	for (i = row + 1, j = col - 1; j >= 0 && i < n; j--, i++) {
-		if (mat[j] & (1U << i)) {
-			return c;
-		}
-	}
+    /* diagonally downward */
+    for (i = row + 1, j = col - 1; j >= 0 && i < n; j--, i++) {
+        if (mat[j] & (1U << i)) {
+            return c;
+        }
+    }
 
-	/* set */
-	mat[col] = 1 << row;
+    /* set */
+    mat[col] = 1 << row;
 
-	if (col == n - 1) {
-		/* completed */
-		c++;
-	} else {
-		/* set remain columns */
-		for (i = n - 1; i >= 0; i--)
-			c += check_and_set(mat, n, i, col + 1);
-	}
-	mat[col] = 0U;
+    if (col == n - 1) {
+        /* completed */
+        c++;
+    } else {
+        /* set remain columns */
+        unsigned int mat_copy[MAX_N];
+        #pragma omp parallel for reduction(+:c) private(i, mat_copy)
+        for (i = n - 1; i >= 0; i--) {
+            memcpy(mat_copy, mat, sizeof(unsigned int) * MAX_N);
+            c += check_and_set(mat_copy, n, i, col + 1);
+        }
+    }
+    mat[col] = 0U;
 
-	return c;
+    return c;
 }
 
 int main(int argc, char *argv[])
 {
-	unsigned int mat[MAX_N], n;
-	int i, count = 0;
+    unsigned int mat[MAX_N], n;
+    int i, count = 0;
 
-	/* check argument */
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s number\n", argv[0]);
-		return -1;
-	}
+    /* check argument */
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s number\n", argv[0]);
+        return 1;
+    }
 
-	/* obtain n */
-	n = atoi(argv[1]);
-	if ((n < 2) || (n > MAX_N)) {
-		fprintf(stderr, "You should specify a number "
-			"between 2 and %d.\n", MAX_N);
-		return -1;
-	}
+    n = atoi(argv[1]);
+    if (n > MAX_N) {
+        fprintf(stderr, "number must be less than or equal to %d\n", MAX_N);
+        return 1;
+    }
 
-	/* initialize */
-	#pragma omp parallel for private(i)
-	for (i = 0; i < n; i++)
-		mat[i] = 0U;
+    #pragma omp parallel for reduction(+:count) private(i)
+    for (i = 0; i < n; i++) {
+        unsigned int mat_copy[MAX_N] = {0};
+        count += check_and_set(mat_copy, n, i, 0);
+    }
 
-	/* query */
-	#pragma omp parallel for private(i) reduction(+:count)
-	for (i = n - 1; i >= 0; i--)
-		count += check_and_set(mat, n, i, 0);
+    printf("Total solutions: %d\n", count);
 
-	printf("Total answer for %d x %d = %d\n", n, n, count);
-
-	return 0;
+    return 0;
 }
